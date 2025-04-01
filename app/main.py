0228f8f7cb58416c2739a2e0cc4752a1ee981fc1
Andrey <90110501+PEMOHTOKOH@users.noreply.github.com>
2025-04-01 19:45:21 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 def copy_file(command: str) -> None:
     cmd = command.split()
-    if len(cmd) == 3:
+    if len(cmd) == 3 and cmd[0] == "cp":
         copy_command = cmd[0]
         file_name_to_copy = cmd[1]
         new_file_name = cmd[2]
     else:
         return
 
-    if file_name_to_copy != new_file_name and copy_command == "cp":
+    if file_name_to_copy != new_file_name:
         try:
             with (open(file_name_to_copy, "r") as file_in,
                   open(new_file_name, "w") as file_out):
